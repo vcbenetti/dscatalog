b595c64925e9684537a0ab819fcf4f8741df260a
Victor Benetti <b3n3771@hotmail.com>
2025-07-08 20:32:23 -0300

    auditin data

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -46,6 +46,12 @@ public class CategoryResource {
         return ResponseEntity.ok().body(dto);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -53,4 +56,18 @@ public class CategoryService {
 
         }
     }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
 }
