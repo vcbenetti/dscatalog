5c50e9ca1a93c6635e7ac8b3f0e8634543a0099e
Victor Benetti <b3n3771@hotmail.com>
2025-07-08 21:10:46 -0300

    pagination

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -8,6 +8,9 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,10 +27,12 @@ public class CategoryService {
 
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
-        List<Category> list = repository.findAll();
-        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-            }
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+        Page<Category> list = repository.findAll(pageRequest);
+        return list.map(x -> new CategoryDTO(x));
+    }
+
+
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
