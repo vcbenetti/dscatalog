39921642c5c0d4fde59bc2f2600fc745db640601
Victor Benetti <b3n3771@hotmail.com>
2025-07-11 21:59:54 -0300

    user crud

---
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscatalog.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
